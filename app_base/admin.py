6af455643eae949f9b172dcd6ebb079f381a8cff
Clifford Laserre <claserre9@gmail.com>
2021-02-08 14:54:37 -0500

    List display

--- app_base/admin.py
+++ app_base/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from app_base.models import Blog
 
-admin.site.register(Blog)
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date_created', 'last_modified', 'is_draft']
+
+
+admin.site.register(Blog, BlogAdmin)
