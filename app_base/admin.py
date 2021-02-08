816dca21dc90da9ff4a7714ae30bd034d88a4623
Clifford Laserre <claserre9@gmail.com>
2021-02-08 15:42:47 -0500

    Costum ordering

--- app_base/admin.py
+++ app_base/admin.py
@@ -5,6 +5,13 @@ from app_base.models import Blog
 
 class BlogAdmin(admin.ModelAdmin):
     list_display = ['title', 'date_created', 'last_modified', 'is_draft']
+    list_filter = ['is_draft']
+    ordering = ('title', '-date_created')
+
+    def get_ordering(self, request):
+        if request.user.is_superuser:
+            return 'title', '-date_created',
+        return 'title',
 
 
 admin.site.register(Blog, BlogAdmin)
