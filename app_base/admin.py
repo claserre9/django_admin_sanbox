9f7ca9648f268de87189881c1ea8f89ac1477bbf
Clifford Laserre <claserre9@gmail.com>
2021-02-08 16:10:20 -0500

    Actions and Date Hierarchy

--- app_base/admin.py
+++ app_base/admin.py
@@ -1,3 +1,5 @@
+
+
 from django.contrib import admin
 
 from app_base.models import Blog
@@ -7,13 +9,21 @@ class BlogAdmin(admin.ModelAdmin):
     list_display = ['title', 'date_created', 'last_modified', 'is_draft']
     list_filter = ['is_draft', 'date_created']
     search_fields = ['title']
-    prepopulated_fields = {'slug':('title',)}
+    prepopulated_fields = {'slug': ('title',)}
     list_per_page = 50
+    actions = ('set_blog_to_published',)
+    date_hierarchy = 'date_created'
 
     def get_ordering(self, request):
         if request.user.is_superuser:
             return 'title', '-date_created',
         return 'title',
+
+    def set_blog_to_published(self, request, queryset):
+        count = queryset.update(is_draft=False)
+        self.message_user(request, f"The {count} selected blogs has been published")
+
+    set_blog_to_published.short_description = "Mark selected blogs as published"
 
 
 admin.site.register(Blog, BlogAdmin)
